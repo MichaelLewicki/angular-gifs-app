7deacc95561570540cce8ba9d3886f2fb32c410e
Michael Lewicki <lewickiclass@gmail.com>
2024-05-26 21:16:29 -0400

    loading animation was added

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'shared-lazy-image',
@@ -19,8 +20,9 @@ export class LazyImageComponent implements OnInit {
   }
 
   onLoad() {
-    console.log('Image loaded');
-    this.hasLoaded = true;
+    setTimeout( () => {
+      this.hasLoaded = true;
+    }, 1000 );
   }
 
 }
